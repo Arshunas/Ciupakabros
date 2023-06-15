e97eb88f03a7c0514e9f100db879e89d27cd4dcd
Sarunas <sarunas.stankevicius@mailbox.org>
2023-06-15 19:13:08 +0300

    014 update1
    Signed-off-by: Sarunas <sarunas.stankevicius@mailbox.org>

--- 014/index.php
+++ 014/index.php
@@ -10,7 +10,26 @@ function getUnique($to)
     return $id;
 }
 
+function randString()
+{
+    $letters = range('a', 'z');
+    $out = '';
+    foreach(range(1, rand(5, 15)) as $_) {
+        $out .= $letters[rand(0, count($letters) - 1 )];
+    }
+    return $out;
+}
+
 $users = array_map(fn($_) => ['user_id' => getUnique(100), 'place_in_row'=> rand(1, 100)], range(1, 30));
+
+usort($users, fn($a, $b) => $a['user_id'] <=> $b['user_id']);
+
+$users = array_map(function($user)
+{
+    $user['name'] = randString();
+    $user['surname'] = randString();
+    return $user;
+}, $users);
 
 echo '<pre>';
 print_r($users);
